ea8436c8bf2f06a2a333d99fcf2e7f973b7402b4
fxj <fxijing@163.com>
2019-08-30 09:28:48 +0800

    框架

---
+++ src/main/java/org/apache/jfcloud/JfcloudApplication.java
@@ -0,0 +1,13 @@
+package org.apache.jfcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JfcloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/apache/jfcloud/JfcloudApplicationTests.java
@@ -0,0 +1,16 @@
+package org.apache.jfcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JfcloudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
